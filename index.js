157554ec9f6490bdefcf5bf99d38bf4a0d3a5af2
o.pochic@gmail.com <o.pochic@gmail.com>
2022-03-27 15:08:02 +0200

    Ma version de Heracles

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const fg = require('./src/Fighter');
+
+let heracles = new fg('🐬 Heracles',20,15,100);
+let biloute = new fg('🐂 Biloute',20,16,100);
+let lion = new fg('🦔 Lion de némée',20,15,100);
+
+console.log('--------------------------------------------');
+console.log('----------- FIGHT STARTS !!! ---------------');
+console.log('--------------------------------------------');
+
+while(heracles.life > 0 && lion.life > 0 ){
+    heracles.fight(lion); //player1 -> donne un kick
+    lion.fight(heracles); //player2 -> donne un kick
+}
+
+console.log('--------------------------------------------');
+if(heracles.life > lion.life){
+    console.log("🏆 🐬 Heracles à gagné !🐬 🏆");
+}else{
+    console.log("🏆 🦔 Lion de Némée à gagné !🦔 🏆");
+}
+
+lion.showInfos();
+heracles.showInfos();
+console.log('--------------------------------------------');
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,43 @@
 /* Fighter class definition */
+
+class Fighter{
+    constructor(name,force, dexterity,life=100){
+        const MAX_LIFE = 100;
+        this.name = name;
+        this.force = force;
+        this.dexterity = dexterity;
+        this.damage = 0;
+        this.life = MAX_LIFE;
+    }
+
+    showInfos(){
+        console.log(`${this.name} à ${this.force} pts (force) / ${this.dexterity} (dexterite) /  ${this.life} (life).`)
+    }
+
+    fight(objEnemy){
+        // calcule de l'esquive : true ou false (random)
+        const isDodge= Math.random() >= 0.5;
+        isDodge ? console.log('Coup esquivé...') : console.log('Coup donné !');
+        
+        //calcule du damage : nombre entre 1 et force de l'ennemy (random)
+        this.damage = Math.floor(Math.random() * objEnemy.force);
+        if(this.damage<=1){
+            this.damage = 1;
+        }
+
+        //console.log(`Chiffre entre 1 et force enemy : ${this.damage }`);
+        this.damage = this.damage - objEnemy.dexterity;
+
+        if(this.damage<=1){
+            this.damage = 1;
+        }
+
+        if(!isDodge){
+            objEnemy.life -= this.damage;
+        }
+                
+    }
+
+}
+
+module.exports = Fighter
